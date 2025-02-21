8f30d23fd5cc8d0288ca01373b5a783041aaf12b
calmulieri <calmulieri@gmail.com>
2025-02-21 17:05:22 -0300

    inicio js

---
+++ clase-1.js
@@ -0,0 +1,10 @@
+/*console.log("Este es mi primer intervención en JS---:")
+/* Inicio de mi programa de prueba */
+/* Usar let o const ----   no usar var para la definición de variables */
+
+let miusuario ="cmulieri" /* con let la variable puede ser mocificada */
+let minumero =prompt("Ingresa tu DNI:") /* prompt permite un input por consola */
+const usuariofinal = miusuario /* const declara variable cuyp valor no puede ser modificado */
+alert(usuariofinal) /* muestra mensaje por consola */
+
+
